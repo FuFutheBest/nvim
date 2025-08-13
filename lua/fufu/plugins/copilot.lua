00b063e00e63507c0b8a27f6032eb8c54557254c
fufu <2741835851@sjtu.edu.cn>
2025-08-13 13:12:32 +0800

    feat: copilot key mapping

--- lua/fufu/plugins/copilot.lua
+++ lua/fufu/plugins/copilot.lua
@@ -6,5 +6,13 @@ return {
 
     keymap.set("n", "<leader>ce", ":Copilot enable<CR>", { desc = "Enable Copilot" })
     keymap.set("n", "<leader>cd", ":Copilot disable<CR>", { desc = "Disable Copilot" })
+
+    -- Use <C-O> in insert mode to accept Copilot suggestions
+    vim.keymap.set("i", "<C-O>", 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+      desc = "Accept Copilot suggestion",
+    })
+    vim.g.copilot_no_tab_map = true
   end,
 }
