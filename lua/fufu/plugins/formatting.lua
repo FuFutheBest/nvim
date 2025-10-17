58182ebe395ec1ecbc16e2b05ba05be7bba86153
fufu <2741835851@sjtu.edu.cn>
2025-10-17 13:05:37 +0800

    feat: add auto formating bounds

--- lua/fufu/plugins/formatting.lua
+++ lua/fufu/plugins/formatting.lua
@@ -5,6 +5,26 @@ return {
     local conform = require("conform")
     local home = os.getenv("HOME")
 
+    local auto_format_filetypes = {
+      lua = true,
+      python = true,
+      cpp = true,
+      c = true,
+      -- elm = true,
+      javascript = true,
+      typescript = true,
+      javascriptreact = true,
+      typescriptreact = true,
+      svelte = true,
+      css = true,
+      html = true,
+      json = true,
+      yaml = true,
+      markdown = true,
+      graphql = true,
+      liquid = true,
+    }
+
     conform.setup({
       formatters_by_ft = {
         javascript = { "prettier" },
@@ -20,7 +40,7 @@ return {
         graphql = { "prettier" },
         liquid = { "prettier" },
         lua = { "stylua" },
-        elm = { "elm-format" },
+        -- elm = { "elm-format" },
         python = { "isort", "black" },
         c = { "clang-format" },
         cpp = { "clang-format" },
@@ -33,11 +53,19 @@ return {
           },
         },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
+
+      format_on_save = function(bufnr)
+        local ft = vim.bo[bufnr].filetype
+        if auto_format_filetypes[ft] then
+          return {
+            lsp_fallback = true,
+            async = false,
+            timeout_ms = 1000,
+          }
+        else
+          return false
+        end
+      end,
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
