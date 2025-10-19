fe9451cd460e59cde571a321987532070ce94b9d
fufu <2741835851@sjtu.edu.cn>
2025-10-19 22:12:03 +0800

    config(plugins): merge jupyter related files

--- lua/fufu/plugins/jupyter.lua
+++ lua/fufu/plugins/jupyter.lua
@@ -1,50 +1,57 @@
 return {
-  -- {
-  "benlubas/molten-nvim",
-  version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
-  dependencies = { "3rd/image.nvim" },
-  build = ":UpdateRemotePlugins",
-  init = function()
-    -- these are examples, not defaults. Please see the readme
-    vim.g.molten_image_provider = "image.nvim"
-    vim.g.molten_output_win_max_height = 20
-    vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
-  end,
 
-  config = function()
-    vim.keymap.set("n", "<leader>mi", function()
-      local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
-      if venv ~= nil then
-        -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
-        venv = string.match(venv, "/.+/(.+)")
-        vim.cmd(("MoltenInit %s"):format(venv))
-      else
-        vim.cmd("MoltenInit python3")
-      end
-    end, { desc = "Initialize Molten for python3", silent = true })
+  {
+    "GCBallesteros/jupytext.nvim",
+    config = true,
+    -- Depending on your nvim distro or config you may need to make the loading not lazy
+    -- lazy=false,
+  },
+  {
+    "benlubas/molten-nvim",
+    version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    dependencies = { "3rd/image.nvim" },
+    build = ":UpdateRemotePlugins",
+    init = function()
+      -- these are examples, not defaults. Please see the readme
+      vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_output_win_max_height = 20
+      vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
+    end,
 
-    vim.keymap.set(
-      "n",
-      "<localleader>e",
-      ":MoltenEvaluateOperator<CR>",
-      { silent = true, desc = "run operator selection" }
-    )
-    vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
-    vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "re-evaluate cell" })
-    vim.keymap.set(
-      "v",
-      "<localleader>r",
-      ":<C-u>MoltenEvaluateVisual<CR>gv",
-      { silent = true, desc = "evaluate visual selection" }
-    )
-    vim.keymap.set("n", "<localleader>rd", ":MoltenDelete<CR>", { silent = true, desc = "molten delete cell" })
-    vim.keymap.set("n", "<localleader>oh", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
-    vim.keymap.set(
-      "n",
-      "<localleader>os",
-      ":noautocmd MoltenEnterOutput<CR>",
-      { silent = true, desc = "show/enter output" }
-    )
-  end,
-  -- },
+    config = function()
+      vim.keymap.set("n", "<leader>mi", function()
+        local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
+        if venv ~= nil then
+          -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
+          venv = string.match(venv, "/.+/(.+)")
+          vim.cmd(("MoltenInit %s"):format(venv))
+        else
+          vim.cmd("MoltenInit python3")
+        end
+      end, { desc = "Initialize Molten for python3", silent = true })
+
+      vim.keymap.set(
+        "n",
+        "<localleader>e",
+        ":MoltenEvaluateOperator<CR>",
+        { silent = true, desc = "run operator selection" }
+      )
+      vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
+      vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "re-evaluate cell" })
+      vim.keymap.set(
+        "v",
+        "<localleader>r",
+        ":<C-u>MoltenEvaluateVisual<CR>gv",
+        { silent = true, desc = "evaluate visual selection" }
+      )
+      vim.keymap.set("n", "<localleader>rd", ":MoltenDelete<CR>", { silent = true, desc = "molten delete cell" })
+      vim.keymap.set("n", "<localleader>oh", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
+      vim.keymap.set(
+        "n",
+        "<localleader>os",
+        ":noautocmd MoltenEnterOutput<CR>",
+        { silent = true, desc = "show/enter output" }
+      )
+    end,
+  },
 }
