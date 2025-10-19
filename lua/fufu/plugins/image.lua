3c2c15a1e83c0a2f984b30f8db637e2c47c2648a
fufu <fufu@fufuemail.com>
2025-10-19 22:02:39 +0800

    feat(plugins): molten and jupytext

--- lua/fufu/plugins/image.lua
+++ lua/fufu/plugins/image.lua
@@ -33,12 +33,12 @@ return {
           enabled = false,
         },
       },
-      max_width = nil,
-      max_height = nil,
-      max_width_window_percentage = nil,
-      max_height_window_percentage = 50,
+      max_width = 100,
+      max_height = 12,
+      max_width_window_percentage = math.huge,
+      max_height_window_percentage = math.huge,
       scale_factor = 1.0,
-      window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+      window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
       window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
       editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
       tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
