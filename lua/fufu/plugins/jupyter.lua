eab366e59229d31f8ed00910887e525212088fdd
fufu <fufu@fufuemail.com>
2025-12-22 00:13:00 +0800

    empty

--- lua/fufu/plugins/jupyter.lua
+++ lua/fufu/plugins/jupyter.lua
@@ -34,23 +34,23 @@ return {
 
       vim.keymap.set(
         "n",
-        "<localleader>e",
+        "<leader>me",
         ":MoltenEvaluateOperator<CR>",
         { silent = true, desc = "run operator selection" }
       )
-      vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
-      vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "re-evaluate cell" })
+      vim.keymap.set("n", "<leader>rl", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
+      vim.keymap.set("n", "<leader>rr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "re-evaluate cell" })
       vim.keymap.set(
         "v",
-        "<localleader>r",
+        "<leader>r",
         ":<C-u>MoltenEvaluateVisual<CR>gv",
         { silent = true, desc = "evaluate visual selection" }
       )
-      vim.keymap.set("n", "<localleader>rd", ":MoltenDelete<CR>", { silent = true, desc = "molten delete cell" })
-      vim.keymap.set("n", "<localleader>oh", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
+      vim.keymap.set("n", "<leader>rd", ":MoltenDelete<CR>", { silent = true, desc = "molten delete cell" })
+      vim.keymap.set("n", "<leader>oh", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
       vim.keymap.set(
         "n",
-        "<localleader>os",
+        "<leader>ms",
         ":noautocmd MoltenEnterOutput<CR>",
         { silent = true, desc = "show/enter output" }
       )
